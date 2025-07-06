549163a7c717cd3f289f76da8ee836e938560f93
unknown <gooogleindo7@gmail.com>
2025-07-06 21:31:34 +0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+
+app.use(express.json());
+app.use(express.static('public'));
+
+app.post('/submit', (req, res) => {
+  const { message, name } = req.body;
+  const msg = { message, name, time: new Date() };
+
+  const filePath = path.join(__dirname, 'messages.json');
+  let messages = [];
+
+  if (fs.existsSync(filePath)) {
+    messages = JSON.parse(fs.readFileSync(filePath));
+  }
+
+  messages.push(msg);
+  fs.writeFileSync(filePath, JSON.stringify(messages, null, 2));
+
+  res.json({ msg: 'Message saved! Thank you.' });
+});
+
+app.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
